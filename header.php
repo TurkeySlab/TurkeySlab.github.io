5256f65104a22224fe337daba0b4080051c97829
TurkeySlab <maxwellwilliams442@gmail.com>
2017-04-18 13:28:36 -0400

    Test of Strcture
    Filese here will be used but need to be updated to a full site. this is
    to syncdomain name to code.

---
+++ header.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link type="text/css" rel="stylesheet" href="header-style.css">
+	<link type="text/css" rel="stylesheet" href="page-style.css">
+	<!--<link rel="SHORTCUT ICON" href="/favicon.ico" />
+	<link rel="icon" href="/favicon.ico" type="image/ico" />-->
+</head>
+
+<body>
+
+<div id="theWholeHeadingBlock">
+	<a href="/"><img src="logo-HTW.png" /></a>
+
+
+	<span id="rightHeadingItem">
+		<a href="who.php"><div id="1">who we are</div></a>
+		<a href="what.php"><div id="2">what we do</div></a>
+		<a href="why.php"><div id="3">why we're better</div></a>
+		<a href="download.php"><div id="4">download</div></a>
+		<a href="support.php"><div id="5">support us</div></a>
+	</span>
+</div>
+
+<div id="everything">
